2bc35075c896618420356ad248d541cd24200bfe
zackhengartner <144271419+zackhengartner@users.noreply.github.com>
2026-08-25 11:25:02 -0400

    Implement Word embeddings (NLP 1/4)

---
+++ model/embeddings.py
@@ -0,0 +1,14 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+    def lookup(self, embeddings: NDArray[np.float64], token_ids: NDArray[np.int64]) -> NDArray[np.float64]:
+        # embeddings: (vocab_size, embed_dim) matrix
+        # token_ids: 1D array of integer token IDs
+        # Return the embedding vectors for the given token IDs
+        # return np.round(your_answer, 5)
+        output = []
+        for token in token_ids:
+            output.append(embeddings[token])
+        return np.round(np.asarray(output),5)
